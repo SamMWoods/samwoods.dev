902fc2b011b99e097fcf77477b41c7de7aafe694
SamMWoods <sammichaelwoods@gmail.com>
2022-07-10 14:03:58 +0100

    init

---
+++ pages/index.js
@@ -0,0 +1,27 @@
+import Head from 'next/head';
+import Image from 'next/image';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Sam Woods</title>
+        <meta name="description" content="Sam Woods Website" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to{' '}
+          <a href="https://upload.wikimedia.org/wikipedia/commons/7/7c/Cima_da_Conegliano%2C_God_the_Father.jpg">
+            Sam Woods
+          </a>{' '}
+          Website
+        </h1>
+      </main>
+
+      <footer></footer>
+    </div>
+  );
+}
